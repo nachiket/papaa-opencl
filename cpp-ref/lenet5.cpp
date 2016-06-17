dc84c1d40e2c9416e2a729287059d75bd63b3228
gplhegde <gplhegde@gmail.com>
2016-06-17 20:42:55 +0800

    lenet2 cpp app done.. testing pending

--- cpp-ref/lenet5.cpp
+++ cpp-ref/lenet5.cpp
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <vector>
+#include <math.h>
 #include "lenet5_model.h"
 
 using namespace cv;
@@ -126,7 +127,91 @@ void InitLenet5Model(ConvLayers &conv, FcLayers &fc) {
 	
 }
 
+// Simple convolution(mathematically it is correlation). Assume no padding = 0
+void convLayer(const vector<Mat> & input, vector<Mat> &output, const ConvParams &cp) {
 
+	for(int n = 0 ;n < cp.noOutputs; n++) {
+		Mat filt;
+		Mat out = Mat::zeros(input[0].rows-cp.filtH+1, input[0].cols-cp.filtW+1, CV_32F);
+		for(int m = 0; m < cp.noInputs; m++) {
+			filter2D(input[m], filt, -1, cp.W[n][m]);
+			add(out, filt, out);
+		}
+		// add bias
+		add(out, cp.bias[n], out);
+		output.push_back(out.clone());
+	}
+}
+
+void maxPoolLayer(vector<Mat> &input, vector<Mat> &output, int winSize, int stride) {
+	int outH, outW;
+	outH = (input[0].rows - winSize) / stride;
+	outW = (input[0].cols - winSize) / stride;
+	Mat out = Mat::zeros(outH, outW, CV_32F);
+	for(int n = 0; n < input.size(); n++) {
+		for(int row = 0; row < input[n].rows; row += stride) {
+			for(int col = 0; col < input[n].cols; col += stride) {
+				Mat temp;
+				Point minLoc, maxLoc;
+				double minVal, maxVal;
+				Rect roi = Rect(col, row, winSize, winSize);
+				input[n](roi).copyTo(temp);
+				minMaxLoc(temp, &minVal, &maxVal);
+				out.at<float>(row, col) = (float)maxVal;
+			}
+		}
+		output.push_back(out.clone());
+	}
+}
+
+
+void reluLayer(vector<float> &input) {
+	for(int e = 0; e < input.size(); e++){
+		if(input[e] < 0.0) {
+			input[e] = 0.0;
+		}
+	}
+}
+
+void innerProductLayer(const vector<float> &input, vector<float> &output, const FcParams &fp) {
+	float acc;
+	for(int n = 0; n < fp.noOutputs; n++) {
+		acc = 0;
+		for(int m = 0; m < fp.noInputs; m++) {
+			acc += input[m] * fp.W.at<float>(n, m);
+		}
+		acc += fp.bias[n];
+		output[n] = acc;
+	}
+}
+
+void innerProductLayer(const vector<Mat> &input, vector<float> &output, const FcParams &fp) {
+	// unroll all the feature maps
+	// There should be a compact way to do this. Lets keep it open
+	assert(fp.noInputs == input.size()*input[0].rows*input[0].cols);
+	vector<float> inArray(fp.noInputs);
+
+	for(int map = 0; map < input.size(); map++) {
+		for(int row = 0; row < input[map].rows; row++) {
+			for(int col = 0; col < input[map].cols; col++) {
+				inArray[map*input[map].rows*input[map].cols + row*input[map].cols + col] = input[map].at<float>(row, col);
+			}
+		}
+	}
+	innerProductLayer(inArray, output, fp);
+}
+
+void softmaxLayer(vector<float> &input, vector<float> &output) {
+	float sum = 0;
+	assert(input.size() == output.size());
+	for(int n = 0; n < input.size(); n++) {
+		output[n] = exp(input[n]);
+		sum += output[n];
+	}
+	for(int n = 0; n < input.size(); n++) {
+		output[n] /= sum;
+	}
+}
 
 int main(int argc, char **argv) {
 
