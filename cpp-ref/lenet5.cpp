0edfc49e4c32d63fbe3ba09b156f2daa6309f77e
gplhegde <gplhegde@gmail.com>
2016-06-18 01:05:51 +0800

    C++ reference app for Lenet5 testing done.. working

--- cpp-ref/lenet5.cpp
+++ cpp-ref/lenet5.cpp
@@ -1,3 +1,12 @@
+/*
+Author: Gopalakrishna Hegde, NTU Singapore
+Date: 17 June 2016
+
+
+Handwritten digit recognition using Lenet5 CNN.
+The network is trained using Caffe and the model is stored as C file
+*/
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
@@ -6,7 +15,6 @@
 #include <stdlib.h>
 #include <iostream>
 #include <stdio.h>
-#include <sys/time.h>
 #include <vector>
 #include <math.h>
 #include "lenet5_model.h"
@@ -122,8 +130,6 @@ void InitLenet5Model(ConvLayers &conv, FcLayers &fc) {
 	fcParam2.noOutputs = IP2_NO_OUTPUTS;
 	populateFcParams(fcParam2, (const float *)ip2_weights, (const float *)ip2_bias);
 	fc.layerParams.push_back(fcParam2);
-	cout << fc.layerParams[0].bias[0] << endl;
-	cout << fc.layerParams[1].bias[0] << endl;
 	
 }
 
@@ -134,32 +140,45 @@ void convLayer(const vector<Mat> & input, vector<Mat> &output, const ConvParams 
 		Mat filt;
 		Mat out = Mat::zeros(input[0].rows-cp.filtH+1, input[0].cols-cp.filtW+1, CV_32F);
 		for(int m = 0; m < cp.noInputs; m++) {
-			filter2D(input[m], filt, -1, cp.W[n][m]);
+			// 2D correlation
+			filter2D(input[m], filt, -1, cp.W[n][m], Point(-1, -1), 0, BORDER_CONSTANT);
+			// neglect the output corresponding to padded border
+			filt = filt.colRange((cp.W[n][m].cols-1)/2, filt.cols - cp.W[n][m].cols/2).rowRange((cp.W[n][m].rows-1)/2, filt.rows -
+				cp.W[n][m].rows/2);
+			// add output from all input channels
 			add(out, filt, out);
 		}
 		// add bias
 		add(out, cp.bias[n], out);
+		// store a deep copy
 		output.push_back(out.clone());
 	}
 }
 
-void maxPoolLayer(vector<Mat> &input, vector<Mat> &output, int winSize, int stride) {
+void maxPoolLayer(const vector<Mat> &input, vector<Mat> &output, int winSize, int stride) {
 	int outH, outW;
-	outH = (input[0].rows - winSize) / stride;
-	outW = (input[0].cols - winSize) / stride;
+	// this is going to be output map size
+	outH = (input[0].rows - winSize ) / stride + 1;
+	outW = (input[0].cols - winSize ) / stride + 1;
+
 	Mat out = Mat::zeros(outH, outW, CV_32F);
+
 	for(int n = 0; n < input.size(); n++) {
 		for(int row = 0; row < input[n].rows; row += stride) {
 			for(int col = 0; col < input[n].cols; col += stride) {
 				Mat temp;
 				Point minLoc, maxLoc;
 				double minVal, maxVal;
+				// this is the pooling window region
 				Rect roi = Rect(col, row, winSize, winSize);
 				input[n](roi).copyTo(temp);
+				// find the max value in the pooling region
 				minMaxLoc(temp, &minVal, &maxVal);
-				out.at<float>(row, col) = (float)maxVal;
+				// store the max value
+				out.at<float>(row/stride, col/stride) = (float)maxVal;
 			}
 		}
+		// store a deep copy
 		output.push_back(out.clone());
 	}
 }
@@ -177,15 +196,19 @@ void innerProductLayer(const vector<float> &input, vector<float> &output, const 
 	float acc;
 	for(int n = 0; n < fp.noOutputs; n++) {
 		acc = 0;
+		// dot product
 		for(int m = 0; m < fp.noInputs; m++) {
 			acc += input[m] * fp.W.at<float>(n, m);
 		}
+		// add bias
 		acc += fp.bias[n];
-		output[n] = acc;
+		// store the deep copy
+		output.push_back(acc);
 	}
 }
 
 void innerProductLayer(const vector<Mat> &input, vector<float> &output, const FcParams &fp) {
+
 	// unroll all the feature maps
 	// There should be a compact way to do this. Lets keep it open
 	assert(fp.noInputs == input.size()*input[0].rows*input[0].cols);
@@ -203,9 +226,8 @@ void innerProductLayer(const vector<Mat> &input, vector<float> &output, const Fc
 
 void softmaxLayer(vector<float> &input, vector<float> &output) {
 	float sum = 0;
-	assert(input.size() == output.size());
 	for(int n = 0; n < input.size(); n++) {
-		output[n] = exp(input[n]);
+		output.push_back(exp(input[n]));
 		sum += output[n];
 	}
 	for(int n = 0; n < input.size(); n++) {
@@ -213,6 +235,40 @@ void softmaxLayer(vector<float> &input, vector<float> &output) {
 	}
 }
 
+int lenet5App(Mat &input, const ConvLayers &convModel, const FcLayers &fcModel) {
+	vector<Mat> inVec;
+	vector<Mat> conv1Out, conv2Out, pool1Out, pool2Out;
+	vector<float> ip1Out, ip2Out, prob;
+	// conv layer requires vector of mat.
+	inVec.push_back(input);
+	// conv layer 1
+	convLayer(inVec, conv1Out, convModel.layerParams[0]);
+	// pool layer 1
+	maxPoolLayer(conv1Out, pool1Out, 2, 2);
+	// conv layer 2
+	convLayer(pool1Out, conv2Out, convModel.layerParams[1]);
+	// pool layer 2
+	maxPoolLayer(conv2Out, pool2Out, 2, 2);
+
+	// inner product 1
+	innerProductLayer(pool2Out, ip1Out, fcModel.layerParams[0]);
+	// ReLU layer
+	reluLayer(ip1Out);
+	// inner product 2
+	innerProductLayer(ip1Out, ip2Out, fcModel.layerParams[1]);
+
+	softmaxLayer(ip2Out, prob);
+
+	cout << "-----------Output probabilities--------" << endl;
+	for(int p = 0; p < prob.size(); p++) {
+		cout << prob[p] << ",  ";
+	}
+	cout << endl << endl;
+	
+	return distance(prob.begin(), max_element(prob.begin(), prob.end()));
+
+	
+}
 int main(int argc, char **argv) {
 
 	if(argc < 2) {
@@ -222,16 +278,24 @@ int main(int argc, char **argv) {
 
 	Mat input = imread(argv[1], IMREAD_GRAYSCALE);
 
-	//namedWindow("Input image", CV_WINDOW_AUTOSIZE);
-	//imshow("Input image", input);
+	// Input normalization to make it in the range [0, 1], assuming the input image to be in the range [0, 255]
+	Mat normInput;
+	input.convertTo(normInput, CV_32F);
+	normInput = normInput/255;
 
-	// Model
+	// Model storage 
 	ConvLayers convModel;
 	FcLayers fcModel;
+
 	// Model initialization
 	InitLenet5Model(convModel, fcModel);
 
+	// Forward pass of the network
+	cout << "Starting the forward pass...." << endl;
+	int predNo = lenet5App(normInput, convModel, fcModel);
+
+	cout << "The digit in the image is = " << predNo << endl;
 	cout<<"Application complete"<<endl;
-	//waitKey(0);
+
 	return 0;
 }
